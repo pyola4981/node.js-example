f35b78990aadd4a59e65240a58cb7dfa30314ea8
pyola4981 <pyola4981@naver.com>
2022-05-11 21:47:05 +0900

    test

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
-var express = require('express'); // 설치한 express module을 불러와서 변수(express)에 담습니다.
-var app = express(); //express를 실행하여 app object를 초기화 합니다.
+var express = require('express');
+var app = express();
 
-app.get('/', (req, res) => { // '/' 위치에 'get'요청을 받는 경우,
-  res.send('Hello World!'); // "Hello World!"를 보냅니다.
+app.set('view engine','ejs'); // 1
+app.use(express.static(__dirname + '/public'));
+
+app.get('/hello', function(req,res){ // 2
+  res.render('hello', {name:req.query.nameQuery});
 });
 
-var port = 3000; // 사용할 포트 번호를 port 변수에 넣습니다. 
-app.listen(port, function(){ // port변수를 이용하여 3000번 포트에 node.js 서버를 연결합니다.
-  console.log('server on! http://localhost:'+port); //서버가 실행되면 콘솔창에 표시될 메세지입니다.
+app.get('/hello/:nameParam', function(req,res){ // 3
+  res.render('hello', {name:req.params.nameParam});
+});
+
+var port = 3000;
+app.listen(port, function(){
+  console.log('server on! http://localhost:'+port);
 });
